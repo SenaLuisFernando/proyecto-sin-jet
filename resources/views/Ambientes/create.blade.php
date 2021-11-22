a7cee42f5063693aac4a15238ef7ce90afab5048
Daniel <danielledezmad9@gmail.com>
2021-11-22 02:02:01 -0400

    asd

--- resources/views/Ambientes/create.blade.php
+++ resources/views/Ambientes/create.blade.php
@@ -10,10 +10,7 @@
     
 <form action="/Ambientes" method="POST">
   @csrf
- <!-- <div class="mb-3">
-    <label for="" class="form-label">Id</label>
-    <input id="Id" name="Id" type="number" class="form-control" tabindex="1">    
-  </div>-->
+ <!-- hola eh camnbiado el codigo deberias ver esto  se esta modificando algo uno dos </div>-->
    
 
     <label for="" class="form-label">Cuarto</label>
